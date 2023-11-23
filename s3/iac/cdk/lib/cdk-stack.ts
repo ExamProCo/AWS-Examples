620145f81758007131ab51d8b26acf4cb654ef8c
Andrew Brown <andrew@monsterboxpro.com>
2023-11-23 16:28:38 +0000

    simple s3 cdk example

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,10 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    const bucket = new s3.Bucket(this, 'MyBucket');
+  }
+}
